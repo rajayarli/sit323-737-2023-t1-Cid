a345fb6362c794b432a1c649a7aea21c76538a6c
raja ramesh <rajarameshy1@gmail.com>
2023-05-30 15:48:24 +1000

    robot message

--- server.js
+++ server.js
@@ -7,9 +7,9 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hey human');
+  res.send('Hey Robots!');
 });
 
 app.listen(PORT, HOST, () => {
   console.log(`Running on http://${HOST}:${PORT}`);
-});
+})
